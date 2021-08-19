9d244e4807ababa0ca2bef5a5eec2c42838bd006
Anton <vanasscheanton@gmail.com>
2021-08-19 16:36:49 +0200

    Update

--- setup/fedora/packages.sh
+++ setup/fedora/packages.sh
@@ -27,7 +27,7 @@ cd .. && rm -rf fonts
 message info "Installing vscode..."
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
-dnf check-update
+sudo dnf update -y
 sudo dnf install code
 
 message info "Installing Transmission & Transgui..."
