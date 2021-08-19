8dcb966812677f6be1789970c66214f4369a4be5
Anton <vanasscheanton@gmail.com>
2021-08-19 18:58:40 +0200

    Aesthetics

--- setup/ip.sh
+++ setup/ip.sh
@@ -2,14 +2,14 @@
 
 # This script will setup a local static IP address.
 
-message quest "IP address (example: 192.168.0.100):"
+message quest "IP address (example: 192.168.0.100): "
 read -p " " IP
 
-message quest "Gateway (example: 192.168.0.1):"
+message quest "Gateway (example: 192.168.0.1): "
 read -p " " GATEWAY
 
 nmcli dev status
-message quest "Connection:"
+message quest "Connection: "
 read -p " " CONNECTION
 
 message info "Changing IP from DHCP to STATIC..."
@@ -17,7 +17,7 @@ nmcli con mod "$CONNECTION" ipv4.addresses $IP/24
 nmcli con mod "$CONNECTION" ipv4.gateway $GATEWAY
 
 message info "Select a DNS hosting service."
-message quest "(1) pihole (192.168.0.100)\n             (2) google (8.8.8.8)\n             (3) cloudflare (1.1.1.1)\n        [1/2/3]"
+message quest "(1) pihole (192.168.0.100)\n             (2) google (8.8.8.8)\n             (3) cloudflare (1.1.1.1)\n      [1/2/3]: "
 read -p " " DNS
     case $DNS in
       1) nmcli con mod  "$CONNECTION" ipv4.dns '192.168.0.100';;
