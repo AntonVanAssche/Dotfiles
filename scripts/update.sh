440d9aae24e41783e4c350c61b4f429705849bf9
Anton <vanasscheanton@gmail.com>
2022-01-17 14:48:29 +0100

    Simplify update.sh

--- scripts/update.sh
+++ scripts/update.sh
@@ -2,41 +2,28 @@
 
 # Simple script to update your system.
 
-light_cyan="\e[96m"
-red="\e[31m"
+lightCyan="\e[96m"
 reset="\e[39m"
-purple="\e[95m"
 
 echo
-echo -e "[i] step 1: ${light_cyan}repairing system${reset}"
-	echo -e "[i] ${red}sudo apt-get install -f${reset}"
-	sudo apt-get install -f
+echo -e "[i] ${lightCyan}repairing system${reset}"
+sudo apt-get install -f
 
 echo
-echo -e "[i] step 2: ${light_cyan}update apt cache${reset}"
-	echo -e "[i] ${red}sudo apt-get update${reset}"
-   	sudo  apt-get update
+echo -e "[i] ${lightCyan}update apt cache${reset}"
+sudo  apt-get update
 
 echo
-echo -e "[i] step 3: ${light_cyan}upgrade packages${reset}"
-	echo -e "[i] ${red}sudo apt-get upgrade${reset}"
-	sudo apt-get upgrade
+echo -e "[i] step 3: ${lightCyan}upgrade packages${reset}"
+sudo apt-get upgrade
 
 echo
-echo -e "[i] step 4: ${light_cyan}clean up${reset}"
-	echo -e "[i] ${red}sudo apt-get autoclean${reset}"
-	sudo apt-get autoclean
+echo -e "[i] ${lightCyan}clean up${reset}"
+sudo apt-get autoclean
 
 echo
-echo -e "[i] ${light_cyan}Done!${reset}"
-
-echo
-echo -e "[?] ${light_cyan}Do you want to reboot the system?${reset}"
-        read -p "Reboot? [y/n] " reboot
-        if [[ $reboot = y || $reboot = Y ]] ; then
-                echo -e "[!]${red} System will reboot!${reset}"
-                sleep 2
-                sudo systemctl reboot
-        elif [[ $reboot = n || $reboot = N ]] ; then
-                echo -e "[i]${light_cyan} System won't rebooten!${reset}"
-        fi
+echo -e "[?] ${lightCyan}Do you want to restart the system?${reset}"
+read -p "Reboot? [y/N] " restart
+if [[ $restart = "y" || $restart = "Y" ]] ; then
+   sudo systemctl reboot
+fi
