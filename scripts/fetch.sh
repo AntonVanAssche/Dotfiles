0a25010fdca8fdadde65dcfebd6c063fe862ec28
Anton <vanasscheanton@gmail.com>
2022-01-17 14:48:56 +0100

    Remove unused variables

--- scripts/fetch.sh
+++ scripts/fetch.sh
@@ -6,12 +6,7 @@
 if [ -x "$(command -v tput)" ]; then
 	bold="$(tput bold)"
 	black="$(tput setaf 0)"
-	red="$(tput setaf 1)"
-	green="$(tput setaf 2)"
 	yellow="$(tput setaf 3)"
-	blue="$(tput setaf 4)"
-	magenta="$(tput setaf 5)"
-	cyan="$(tput setaf 6)"
 	white="$(tput setaf 7)"
 	reset="$(tput sgr0)"
 fi
