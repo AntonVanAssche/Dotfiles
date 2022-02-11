36d33da25eb77e653a213f79198c4ac8d5f5cefd
Anton <vanasscheanton@gmail.com>
2022-02-11 15:18:00 +0100

    Swap order

--- setup/main.sh
+++ setup/main.sh
@@ -29,14 +29,6 @@ function Main() {
    message info "Installing packages..."
    source "$dotfilesDir"/setup/packages.sh
 
-   # Ask to configure a static ip-adress.
-   message quest "Do you want to setup a static ip address? [y/N] "
-   read -r ipYN
-   case $ipYN in
-      [Yy]) source "$dotfilesDir"/setup/ip.sh;;
-      *) message info "Script won't configure network settings";;
-   esac
-
    # If Gnome-shell is detected ask to setup.
    if [[ $(command -v gnome-shell) ]]; then
       message info "Gnome-shell detected."
@@ -48,12 +40,13 @@ function Main() {
       esac
    fi
 
-   # If done ask to reboot.
-   message quest "Do you want to reboot the system? [y/N] "
-   read -r restart
-   if [[ $restart = [Yy] ]] ; then
-      sudo systemctl reboot
-   fi
+   # Ask to configure a static ip-adress.
+   message quest "Do you want to setup a static ip address? [y/N] "
+   read -r ipYN
+   case $ipYN in
+      [Yy]) source "$dotfilesDir"/setup/ip.sh;;
+      *) message info "Script won't configure network settings";;
+   esac
 }
 
 time Main 2>&1 | tee -a "$logFile"
