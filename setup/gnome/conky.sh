784aeb8fc20448d61cd53655d0df260dac098911
Anton <vanasscheanton@gmail.com>
2022-05-27 19:48:51 +0200

    Add now-clocking module

--- setup/gnome/conky.sh
+++ setup/gnome/conky.sh
@@ -3,9 +3,7 @@
 printf "%b" "\n${blue} • Conky${normal}\n"
 
 InstallPackage "conky" "Installing conky"
-Execute "git clone https://github.com/AntonVanAssche/now-clocking" "Cloning now-clocking conky widget repo"
-Execute "./now-clocking/scripts/download-fonts.sh" "Installing fonts"
-Execute "mkdir -p $HOME/.config/conky/ && mv now-clocking $HOME/.config/conky/" "Installing now-clocking conky widget"
+Execute "$HOME/.config/conky/now-clocking/scripts/download-fonts.sh" "Installing fonts"
 
 StartConky() {
     conky -q -c "$HOME/.config/conky/now-clocking/conky/np.lua" -d &> /dev/null
