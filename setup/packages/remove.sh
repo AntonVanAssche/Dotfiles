22b9e2698e572181e62c4759e78f6850ed03bc80
Anton <vanasscheanton@gmail.com>
2022-07-05 14:56:34 +0200

    Add more packages to remove

--- setup/packages/remove.sh
+++ setup/packages/remove.sh
@@ -2,9 +2,44 @@
 
 printf "%b" "\n${blue} • Remove Packages${normal}\n"
 
-packagesToRemove=(*libreoffice* rhythmbox gnome-tour gnome-boxes *qemu* gnome-photos)
+packagesToRemove=(
+   # LibreOffice Stuff.
+   libreoffice-calc
+   libreoffice-core
+   libreoffice-draw
+   libreoffice-data
+   libreoffice-emailmerge
+   libreoffice-impress
+   libreoffice-filters
+   libreoffice-graphicfilter
+   libreoffice-gtk3
+   libreoffice-help-en
+   libreoffice-langpack-en
+   libreoffice-ogltrans
+   libreoffice-opensymbol-fonts
+   libreoffice-pdfimport
+   libreoffice-pyuno
+   libreoffice-ure
+   libreoffice-ure-common
+   libreoffice-writer
+   libreoffice-x11
+   libreoffice-xsltfilter
+   unoconv
+
+   # Rythembox.
+   rhythmbox
+
+   # Gnome stuff I don't use.
+   gnome-tour
+   gnome-boxes
+   gnome-photos
+   gnome-connections
+
+   # Fedora stuff I don't use.
+   mediawriter
+)
 
 for packageToRemove in "${packagesToRemove[@]}";
 do
-    RemovePackage "${packageToRemove}" "Removing ${packageToRemove}"
+   RemovePackage "${packageToRemove}" "Removing ${packageToRemove}"
 done
