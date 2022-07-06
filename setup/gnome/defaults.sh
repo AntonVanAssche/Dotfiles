12f3436ded15ccea040927f7cd2ca2519982e34f
Anton <vanasscheanton@gmail.com>
2022-07-06 12:19:25 +0200

    Set alacritty as the default terminal

---
+++ setup/gnome/defaults.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Set alacritty as the default terminal.
+Execute "gsettings set org.gnome.desktop.default-applications.terminal exec alacritty" "Setting alacritty as the default terminal"
+
