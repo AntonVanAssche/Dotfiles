abc6fbc84f72322a784c7693b94359d398b3e125
Anton <vanasscheanton@gmail.com>
2022-07-06 12:41:53 +0200

    Add header for default apps script

--- setup/gnome/defaults.sh
+++ setup/gnome/defaults.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+printf "%b" "\n${blue} • Default apps${normal}\n"
+
 # Set alacritty as the default terminal.
 Execute "gsettings set org.gnome.desktop.default-applications.terminal exec alacritty" "Setting alacritty as the default terminal"
 
