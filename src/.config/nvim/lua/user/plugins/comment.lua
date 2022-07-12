1e2498af2fc04c8e7537a9b6895084e1a843867e
Anton <vanasscheanton@gmail.com>
2022-07-12 18:24:16 +0200

    Fix Commenter config

--- src/.config/nvim/lua/user/plugins/comment.lua
+++ src/.config/nvim/lua/user/plugins/comment.lua
@@ -1,19 +1,35 @@
 local comment = require "Comment"
 
 comment.setup {
-   pre_hook = function(ctx)
-      local U = require "Comment.utils"
+   padding = true,
 
-      local location = nil
-      if ctx.ctype == U.ctype.block then
-         location = require("ts_context_commentstring.utils").get_cursor_location()
-      elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
-         location = require("ts_context_commentstring.utils").get_visual_start_location()
-      end
+   sticky = true,
 
-      return require("ts_context_commentstring.internal").calculate_commentstring {
-         key = ctx.ctype == U.ctype.line and "__default" or "__multiline",
-         location = location,
-      }
-   end,
+   ignore = nil,
+
+   toggler = {
+      line = 'gcc',
+      block = 'gbc',
+   },
+
+   opleader = {
+      line = 'gc',
+      block = 'gb',
+   },
+
+   extra = {
+      above = 'gcO',
+      below = 'gco',
+      eol = 'gcA',
+   },
+
+   mappings = {
+      basic = true,
+      extra = true,
+      extended = false,
+   },
+
+   pre_hook = nil,
+   post_hook = nil,
 }
+
