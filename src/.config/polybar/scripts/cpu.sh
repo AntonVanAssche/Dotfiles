f550dcb05916493ce3b1a6bf38fd42e132d6ab19
Anton <vanasscheanton@gmail.com>
2022-07-22 22:34:09 +0200

    Fix icon spacing

--- src/.config/polybar/scripts/cpu.sh
+++ src/.config/polybar/scripts/cpu.sh
@@ -3,5 +3,5 @@
 ICON=""
 CPU_USAGE="$(grep 'cpu ' /proc/stat | awk -v OFMT="%5.2f" '{usage=(($2+$4)*100/($2+$4+$5))} END {print usage}')"
 
-printf '%s  %s%%' "$ICON" "$CPU_USAGE"
+printf '%s %s%%' "$ICON" "$CPU_USAGE"
 
