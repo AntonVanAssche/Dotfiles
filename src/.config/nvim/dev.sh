6a76453681366a0aeaa2658e6b1fffb80037cfac
Anton <vanasscheanton@gmail.com>
2022-12-28 13:23:08 +0100

    Fix `dev.sh` for testing nvim config

--- src/.config/nvim/dev.sh
+++ src/.config/nvim/dev.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
+# Make sure you execute this script from within this directory (src/.config/nvim).
 mv ~/.config/nvim/ ~/.config/nvim-backup/
-ls -s "$(pwd)" ~/.config/nvim/
+ln -s "$(pwd)" ~/.config/
+ls -la ~/.config/nvim/
