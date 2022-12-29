7919a7e14d7bfa9171019d9bbbfaf4f70be83b5a
Anton <vanasscheanton@gmail.com>
2022-12-29 20:06:25 +0100

    Fix unquoting on lines 254 and 255

--- setup.sh
+++ setup.sh
@@ -251,8 +251,8 @@ Main() {
 
     printf "%b" "\n${blue} • Install rpmfusion ${normal}\n"
 
-    InstallPackage "https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-"$(rpm -E %fedora)".noarch.rpm" "Installing rpmfusion-free"
-    InstallPackage "https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-"$(rpm -E %fedora)".noarch.rpm" "Installing rpmfusion-nonfree"
+    InstallPackage "https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm" "Installing rpmfusion-free"
+    InstallPackage "https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm" "Installing rpmfusion-nonfree"
 
     printf "%b" "\n${blue} • Update system${normal}\n"
     Update
