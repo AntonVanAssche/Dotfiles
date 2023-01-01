41e83ce9c28c4c80e5846489b82eb253f3d6aadf
Anton <vanasscheanton@gmail.com>
2023-01-01 12:53:30 +0100

    Remove date-time-inserter keymaps as they are now handled by the plugin

--- src/.config/nvim/after/plugin/date-time-inserter.lua
+++ src/.config/nvim/after/plugin/date-time-inserter.lua
@@ -3,19 +3,15 @@ if not date_time_inserter_status_ok then
     date_time_inserter = {}
 end
 
-local keymap = vim.keymap.set
-local opts = {
-    noremap = true,
-    silent = true,
-}
-
 date_time_inserter.setup {
-    date_format = 'DDMMYYYY', -- Arange the letter in the order you want the date to appear.
-    time_format = 24,         -- 12 or 24.
-    show_seconds = false,     -- true or false.
+    date_format = 'DDMMYYYY',               -- Arange the letter in the order you want the date to appear.
+    time_format = 24,                       -- 12 or 24.
+    show_seconds = true,                    -- true or false.
 
-    date_separator = '-',     -- Character to separate the date.
+    date_separator = '-',                   -- Character to separate the date.
+
+    insert_date_map = '<leader>dt',         -- Keymap to insert the date in normal mode.
+    insert_time_map = '<leader>tt',         -- Keymap to insert the time in normal mode.
+    insert_date_time_map = '<leader>dtt',   -- Keymap to insert the date and time in normal mode.
+
 }
-
-keymap('n', '<leader>dt', date_time_inserter.insert_date, opts)
-keymap('n', '<leader>tt', date_time_inserter.insert_time, opts)
