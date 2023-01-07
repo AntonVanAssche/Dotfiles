5078742e5dec7faf825d871716375ec1f831a4c1
Anton <vanasscheanton@gmail.com>
2023-01-07 19:39:23 +0100

    Display comments in bold and in italic

--- src/.config/nvim/after/plugin/colors.lua
+++ src/.config/nvim/after/plugin/colors.lua
@@ -4,9 +4,12 @@ function SetColorscheme(color)
 	color = color or "onedark"
 	vim.cmd.colorscheme(color)
 
+    -- Set the background to none so that the background color is transparent
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 
+    -- Set the comments to 'bold', 'italic' and 'grey'.
+    vim.api.nvim_set_hl(0, "Comment", { fg = '#545A65', bold = true, italic = true })
 end
 
 SetColorscheme()
