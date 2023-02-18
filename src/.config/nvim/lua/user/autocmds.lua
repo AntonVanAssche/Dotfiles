a3dc36e68074641773d52edc0a65a6cd9f99116e
Anton <vanasscheanton@gmail.com>
2023-02-18 17:37:25 +0100

    Add js, html, LaTeX skel files

--- src/.config/nvim/lua/user/autocmds.lua
+++ src/.config/nvim/lua/user/autocmds.lua
@@ -12,3 +12,21 @@ vim.api.nvim_create_autocmd('BufNewFile', {
 	pattern = { '*.py' },
 	group = skel_group,
 })
+
+vim.api.nvim_create_autocmd('BufNewFile', {
+	command = '0r ~/.config/nvim/skeletons/skel.tex',
+	pattern = { '*.tex' },
+	group = skel_group,
+})
+
+vim.api.nvim_create_autocmd('BufNewFile', {
+	command = '0r ~/.config/nvim/skeletons/skel.html',
+	pattern = { '*.html' },
+	group = skel_group,
+})
+
+vim.api.nvim_create_autocmd('BufNewFile', {
+	command = '0r ~/.config/nvim/skeletons/skel.js',
+	pattern = { '*.js' },
+	group = skel_group,
+})
