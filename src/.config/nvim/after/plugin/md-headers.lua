d84ae14c56cba132fea6c95a5a048a955a990d04
Anton <vanasscheanton@gmail.com>
2023-10-21 21:38:57 +0200

    Set the higlight of the md-headers popup window to blue

--- src/.config/nvim/after/plugin/md-headers.lua
+++ src/.config/nvim/after/plugin/md-headers.lua
@@ -6,3 +6,10 @@ local opts = { silent = true }
 -- Set keymap
 keymap('n', '<leader>mdh', ':MarkdownHeaders<CR>', opts)
 keymap('n', '<leader>mdhc', ':MarkdownHeadersClosest<CR>', opts)
+
+do
+    vim.cmd [[
+        highlight! MarkdownHeadersTitle guifg=#61afef
+        highlight! MarkdownHeadersBorder guifg=#61afef
+    ]]
+end
