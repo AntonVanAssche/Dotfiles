0165f8bb8c2284cfe15b4795e32c74de84983885
Anton <vanasscheanton@gmail.com>
2023-11-03 17:19:53 +0100

    Remove setup scripts

--- setup/dotfiles/dirs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-printf "%b" "\n${blue} • Create directories${normal}\n"
-
-Execute "mkdir -p $HOME/Projects/" "Creating $HOME/Projects/"
-Execute "mkdir -p $HOME/Downloads/torrents" "Creating $HOME/Downloads/torrents"
-Execute "mkdir -p $HOME/isos" "Creating $HOME/isos"
--- setup/gnome/privacy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-printf "%b" "\n${blue} • Privacy${normal}\n"
-
-# Privacy settings
-Execute "gsettings set org.gnome.desktop.privacy remember-recent-files false" "Applying privacy settings"
