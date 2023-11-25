5fea3c3aa60a716bfc8b7fb542f1d781ce2d46a8
Anton <vanasscheanton@gmail.com>
2023-11-25 01:11:43 +0100

    Remove setup scripts

--- setup/gnome/defaults.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-printf "%b" "\n${blue} • Default apps${normal}\n"
-
-# Set alacritty as the default terminal.
-Execute "gsettings set org.gnome.desktop.default-applications.terminal exec alacritty" "Setting alacritty as the default terminal"
-
--- setup/gnome/theme.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-printf "%b" "\n${blue} • Themes${normal}\n"
-
-# Theme settings
-Execute "gsettings set org.gnome.desktop.interface gtk-theme adw-gtk3-dark" "Applying GTK theme"
-Execute "gsettings set org.gnome.desktop.interface icon-theme Numix-Circle" "Applying icon theme"
-Execute "gsettings set org.gnome.desktop.interface cursor-theme capitaine-cursors" "Applying cursor theme"
-Execute "gsettings set org.gnome.desktop.wm.preferences button-layout :minimize,maximize,close" "Setting button order"
