0a9843becae91646e474593fd0217c4b7d59f4fc
Anton <vanasscheanton@gmail.com>
2024-03-08 19:59:45 +0100

    Align #'s

--- src/.config/nvim/skeletons/skel.sh
+++ src/.config/nvim/skeletons/skel.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -o errexit # Abort on nonzero exit code.
-set -o nounset # Abort on unbound variable.
+set -o errexit  # Abort on nonzero exit code.
+set -o nounset  # Abort on unbound variable.
 set -o pipefail # Don't hide errors within pipes.
-# set -o xtrace   # Enable for debugging.
+# set -o xtrace # Enable for debugging.
