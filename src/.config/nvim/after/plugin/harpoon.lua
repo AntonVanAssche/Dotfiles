45c07c4bd517e311c26be739ccb5702cd67472b8
Anton <vanasscheanton@gmail.com>
2024-03-15 15:20:44 +0100

    Add harpoon remove from list mapping

--- src/.config/nvim/after/plugin/harpoon.lua
+++ src/.config/nvim/after/plugin/harpoon.lua
@@ -6,6 +6,7 @@ end
 harpoon.setup()
 
 vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
+vim.keymap.set("n", "<leader>d", function() harpoon:list():remove() end)
 vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
 vim.keymap.set("n", "<M-1>", function() harpoon:list():select(1) end)
