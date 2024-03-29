4b6f57af9d2581689281c7ab9cb9ed5043817978
Anton <vanasscheanton@gmail.com>
2024-03-29 18:24:03 +0100

    [harpoon] Fix select 5th buffer keymap

--- src/.config/nvim/after/plugin/harpoon.lua
+++ src/.config/nvim/after/plugin/harpoon.lua
@@ -13,7 +13,7 @@ vim.keymap.set("n", "<M-1>", function() harpoon:list():select(1) end)
 vim.keymap.set("n", "<M-2>", function() harpoon:list():select(2) end)
 vim.keymap.set("n", "<M-3>", function() harpoon:list():select(3) end)
 vim.keymap.set("n", "<M-4>", function() harpoon:list():select(4) end)
-vim.keymap.set("n", "<M-3>", function() harpoon:list():select(5) end)
+vim.keymap.set("n", "<M-5>", function() harpoon:list():select(5) end)
 
 vim.keymap.set("n", "<M-S-P>", function() harpoon:list():prev() end)
 vim.keymap.set("n", "<M-S-N>", function() harpoon:list():next() end)
