63de400605f80fdd9ec222696830fbb80162313e
Anton <vanasscheanton@gmail.com>
2024-04-14 23:41:36 +0200

    [nvim] Add yank highlight autocmd

--- src/.config/nvim/lua/anton/core/autocmds.lua
+++ src/.config/nvim/lua/anton/core/autocmds.lua
@@ -30,3 +30,11 @@ vim.api.nvim_create_autocmd("BufNewFile", {
 })
 
 vim.api.nvim_create_autocmd({ "BufLeave" }, { pattern = "*", command = "wa", nested = true })
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = "Highlight when yanking (copying) text",
+  group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
