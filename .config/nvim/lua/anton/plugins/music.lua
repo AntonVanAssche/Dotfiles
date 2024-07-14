8493d4f555a4e8baa41d605b411cc4c66474a566
Anton <vanasscheanton@gmail.com>
2024-07-14 14:39:56 +0200

    [music-controls] Use lazy spec for setting default player

--- .config/nvim/lua/anton/plugins/music.lua
+++ .config/nvim/lua/anton/plugins/music.lua
@@ -1,6 +1,6 @@
 return {
   "AntonVanAssche/music-controls.nvim",
-  config = function()
-    _G.music_controls_default_player = 'spotify'
-  end,
+  opts = {
+    default_player = "spotify"
+  }
 }
