87e9281b00e25cc7cd7986cfae546386ee469796
Anton <vanasscheanton@gmail.com>
2024-09-21 13:43:34 +0200

    [nvim] Remove <CR> completion binding

--- .config/nvim/lua/anton/plugins/lsp.lua
+++ .config/nvim/lua/anton/plugins/lsp.lua
@@ -135,7 +135,6 @@ return {
         ["<C-b>"] = cmp.mapping.scroll_docs(-4),
         ["<C-f>"] = cmp.mapping.scroll_docs(4),
         ["<C-e>"] = cmp.mapping.abort(),
-        ["<CR>"] = cmp.mapping.confirm({ select = true }),
         ["<C-Space>"] = cmp.mapping.complete(),
       }),
       sources = cmp.config.sources({
